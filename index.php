4c997d41ccc4e12c03210cc03392230ba8d6451a
poonkodi <ipoonkodibe@gmail.com>
2020-12-17 20:37:56 +0530

    log to loggly.

--- index.php
+++ index.php
@@ -6,37 +6,13 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
-// use Monolog\Handler\StreamHandler;
-// use Monolog\Logger;
+use Monolog\Handler\LogglyHandler;
+use Monolog\Formatter\LogglyFormatter;
+use Monolog\Logger;
 
-// $logger = new Logger('main');
-// $logger->pushHandler(new StreamHandler(__DIR__ . '/error.log', Logger::DEBUG));
+$log = new Logger('corePhp');
+$log->pushHandler(new LogglyHandler('<TOKEN>/tag/monolog', Logger::INFO));
 
-// $logger->info('First message');
-
-use Analog\Analog;
-
-Analog::handler (function ($message) {
-
-$headers = array('Content-Type: application/json');
-
-$url = "https://poonkodi.loggly.com/inputs/85253272-4a0f-4c06-870c-df194ed770be";
-
- 
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, $url);
-
-curl_setopt($ch, CURLOPT_POST, true);
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, "hi");
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-curl_exec($ch);
-});
+$log->info('test logs to loggly');
 
 echo 'done';
