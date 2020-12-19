c9e0b898b2946d5bac8212a3cbb246f087cc8571
poonkodi <ipoonkodibe@gmail.com>
2020-12-19 19:41:43 +0530

    api call.

--- index.php
+++ index.php
@@ -10,9 +10,24 @@ use Monolog\Handler\LogglyHandler;
 use Monolog\Formatter\LogglyFormatter;
 use Monolog\Logger;
 
-$log = new Logger('corePhp');
-$log->pushHandler(new LogglyHandler('<TOKEN>/tag/monolog', Logger::INFO));
+$ch3 = curl_init();
+curl_setopt($ch3, CURLOPT_URL, 'https://pearlthoughts.stoplight.io/mocks/pearlthoughts/api-app-monitoring-mock/4380658/settings/failure');
+curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch3, CURLOPT_CUSTOMREQUEST, 'GET');
+$result = curl_exec($ch3);
+if (curl_errno($ch3)) {
+    echo 'Error:' . curl_error($ch3);
+    $log = new Logger('corePhpDec19');
+    $log->pushHandler(new LogglyHandler('eb90235e-db33-4a6a-9203-c3e13e0b4d65/tag/monolog', Logger::ERROR));
 
-$log->info('test logs to loggly');
+    $log->error(curl_error($ch3));
+}
+curl_close($ch3);
+$data=json_decode($result, true); 
+if($data['message']) {             
+    $log = new Logger('corePhpDec19Success');
+    $log->pushHandler(new LogglyHandler('eb90235e-db33-4a6a-9203-c3e13e0b4d65/tag/monolog', Logger::INFO));
 
+    $log->info($data['message']);
+}
 echo 'done';
